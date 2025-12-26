b07ed46039a394a88aa41fe6cc69cd53ce0fa157
OWAIS MANZOOR <owaismanzoorkgp@gmail.com>
2025-12-26 19:11:17 +0530

    Update Item.jsx

--- Full_Stack_Ecommerce/frontend/src/Components/Item/Item.jsx
+++ Full_Stack_Ecommerce/frontend/src/Components/Item/Item.jsx
@@ -6,23 +6,11 @@ import { backend_url, currency } from '../../App'
 const Item = (props) => {
   return (
     <div className='item'>
-      <Link to={`/product/${props.id}`}>
-        <img
-          onClick={() => window.scrollTo(0, 0)}
-          src={backend_url + props.image}
-          alt="products"
-        />
-      </Link>
-
+      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0, 0)} src={backend_url+props.image} alt="products" /></Link>
       <p>{props.name}</p>
-
       <div className="item-prices">
-        <div className="item-price-new">
-          {currency}{props.new_price}
-        </div>
-        <div className="item-price-old">
-          {currency}{props.old_price}
-        </div>
+        <div className="item-price-new">{currency}{props.new_price}</div>
+        <div className="item-price-old">{currency}{props.old_price}</div>
       </div>
     </div>
   )
@@ -30,4 +18,3 @@ const Item = (props) => {
 
 export default Item
 
-export default Item
